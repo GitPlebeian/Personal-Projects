b1e7dc021401e68a254b75f772ebda41245317c7
Jax <Jaxtubbs@gmail.com>
2017-11-29 16:05:09 -0800

    a;sldkfjad

--- Personal Project/app.js
+++ Personal Project/app.js
@@ -4,6 +4,8 @@ app.use(express.static("public"));
 
 app.get("/:str", function(req,res){
 	var str = req.params.str;
+	console.log("Someone is to page " + str);
+
 	res.send("you went to page " + str)
 	// if(str === "bob"){
 	// 	res.send("we dont talk about bob");
@@ -11,7 +13,9 @@ app.get("/:str", function(req,res){
 });
 
 app.get("*", function(req,res){
-	res.send("You have reached an unknown page.")
+	// res.send("You have reached an unknown page.")
+	res.render("home.ejs")
+	console.log("Someone is connecting");
 });
 
 app.listen(3000, function(){
